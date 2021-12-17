98f51a3f258e268b7fd690ce3ef284dca4094472
Manju2012 <naganurprasad@yahooo.in>
2021-12-17 13:30:06 +0530

    Added Results feature

--- DjangoQuiz/quizes/static/quizes/quiz.js
+++ DjangoQuiz/quizes/static/quizes/quiz.js
@@ -1,8 +1,12 @@
-console.log('Hello')
+console.log('Hello quiz view')
 
 const url = window.location.href
 
 const quizBox = document.getElementById('quiz-box')
+const ScoreBox = document.getElementById('score-box')
+const resultBox = document.getElementById('result-box')
+
+
 // let data
 
 $.ajax({
@@ -20,11 +24,11 @@ $.ajax({
                     </div>
                 `
 
-                answers.forEach(ans=>{
+                answers.forEach(answer=>{
                     quizBox.innerHTML += `
                         <div>
-                            <input type='radio' class='ans' id='${question}-${ans}' name='${question}' value='${ans}'
-                            <label for='${question}'>${ans}</label>
+                            <input type='radio' class='ans' id='${question}-${answer}' name='${question}' value='${answer}'
+                            <label for='${question}'>${answer}</label>
                     `
                 })
             }
@@ -35,3 +39,80 @@ $.ajax({
         console.log(error)
     }
 });
+
+const quizFom = document.getElementById('quiz-form')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
+
+const sentData = () =>  {
+    const elements = [...document.getElementsByClassName('ans')]
+    const data = {}
+    data['csrfmiddlewaretoken'] = csrf[0].value
+    elements.forEach(el=> {
+        if (el.checked) {
+            data[el.name] = el.value
+        }
+        else {
+            if (!data[el.name]) {
+                data[el.name] = null
+            }
+        }
+    })
+
+$.ajax({
+    type:'POST',
+    url: `${url}save/`,
+    data: data,
+    success: function(response){
+        // console.log(response)
+        const results = response.results
+        quizFom.classList.add('not-visble')
+        console.log(results);
+
+        ScoreBox.innerHTML = `${response.passed ? 'congrailations!' : 'Ups..:('} your result is ${response.score.toFixed(2)}%`
+
+        results.forEach(res => {
+            const resDiv = document.createElement('div')
+            for (const [question, resp] of Object.entries(res)){
+                // console.log(question);
+                // console.log(resp);
+                // console.log('******');
+
+                resDiv.innerHTML += question
+                const cls = ['conntaine', 'p-3', 'text-light','h6']
+                resDiv.classList.add(...cls)
+
+                if (resp=='not answered'){
+                    resDiv.innerHTML += `- not answered`
+                    resDiv.classList.add('bgg-danger')
+                }
+                else{
+                    const answer = resp['answere']
+                    const correct = resp['correcct_answer']
+
+                    // console.log(answer, correct);
+                    if (answer == correct){
+                    resDiv.classList.add('bg-success')
+                    resDiv.innerHTML += `answered : $(answer)`
+                    } else{
+                        resDiv.classList.add('bg-danger')
+                        resDiv.innerHTML += ` | corect answer: ${correct}`
+                        resDiv.innerHTML += ` | answered: $(answer)`
+                    }
+                }
+            }
+            // const body = document.getElementsByClassName('BODY')[0]
+            resultBox.append(resDiv)
+        });
+    },
+    error: function(error){
+        console.log(error);
+    }
+});
+
+}
+
+quizFom.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    sentData()
+})
--- DjangoQuiz/quizes/views.py
+++ DjangoQuiz/quizes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Quiz
 from django.views.generic import ListView
 from django.http import JsonResponse
+from questions.models import Question, Answer
+from results.models import Result
 
 
 # Create your views here.
@@ -29,3 +31,53 @@ def quiz_data_view(request, pk):
         'data': questions,
         'time': quiz.time,
     })
+
+
+def save_quiz_view(request, pk):
+    print(request.POST)
+    if request.is_ajax():
+        questions = []
+        data = request.POST
+        data_ = dict(data.lists())
+        print(data_)
+        data_.pop('csrfmiddlewaretoken')
+        print(data_)
+        for k in data_.keys():
+            print('key: ', k)
+            question = Question.objects.get(text=k)
+            questions.append(question)
+        print(questions)
+
+        user = request.user
+        quiz = Quiz.objects.get(pk=pk)
+
+        score = 0
+        multiplier = 100 / quiz.number_of_questions
+        results = []
+        correct_answer = None
+
+        for q in questions:
+            a_selected = request.POST.get(str(q.text))
+            print(('selected', a_selected))
+
+            if a_selected != '':
+                question_answer = Answer.obejcts.filter(question=q)
+                for a in question_answer:
+                    if a_selected == a.text:
+                        if a.correct:
+                            score += 1
+                            correct_answer = a.text
+                        else:
+                            if a.correct:
+                                correct_answer = a.text
+                    results.append({str(q): {'correct_answer': correct_answer}})
+                else:
+                    results.append({str(q): 'not answered'})
+
+            score_ = score * multiplier
+            Result.objects.create(quiz=quiz, user=user, score=score_)
+
+            if score_ >= quiz.required_score_to_pass:
+                return JsonResponse({'passed': True, 'score': score_, 'results': results})
+            else:
+                return JsonResponse({'text': False, 'score': score_, 'results': results})
